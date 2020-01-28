53a0579e1efea5b9b2d90136499806aff8e3a6e4
Remco Kuijpers <remco.kuijpers@hotmail.nl>
2020-01-28 13:35:30 +0100

    Updated end effector code

--- arm_driver/src/end_effector.py
+++ arm_driver/src/end_effector.py
@@ -5,27 +5,38 @@ from time import sleep
 
 class EndEffector():
     def __init__(self):
-        self.arduino = serial.Serial('/dev/ttyUSB0', 9600)
+        self.arduino = serial.Serial('/dev/ttyACM0', 115200)
         
     def sendCommand(self, val):
-        self.arduino.write(str(chr(val)))
+        self.arduino.write(str(val))
+
+    def startDispensing(self):
+        self.sendCommand(2)
+        sleep(0.1)
+
+    def stopDispensing(self):
+        self.sendCommand(3)
+        sleep(0.1)
 
     def startBrushing(self):
         self.sendCommand(1)
+        sleep(0.1)
 
     def stopBrushing(self):
         self.sendCommand(0)
-
-    def startDispensing(self):
-        self.sendCommand("StartDispensing")
-
-    def stopDispensing(self):
-        self.sendCommand("StopDispensing")
+        sleep(0.1)
 
 if __name__ == "__main__":
     EE = EndEffector()
-    while True:
-        EE.startBrushing()
-        sleep(5)
-        EE.stopBrushing()
-        sleep(5)
+    #while True:
+    #    EE.startBrushing()
+    #    print("Start brushing")
+    #    sleep(1)
+    #    EE.startDispensing()
+    #    print("Start dispensing")
+    #    sleep(5)
+    #    EE.stopBrushing()
+    #    EE.stopDispensing()
+    #    print("Stop brushing")
+    #    print("Stop dispensing")
+    #    sleep(1)
