3997ce532f2fa85a853467697ae3d1dbf505747e
Remco Kuijpers <remco.kuijpers@hotmail.nl>
2020-01-31 13:21:22 +0100

    Added demo program and end effector

--- arm_driver/src/send_commands.py
+++ arm_driver/src/send_commands.py
@@ -10,6 +10,7 @@ import yaml
 import getpass
 
 from arm_driver.srv import reset_errors, power, teach_position
+from end_effector import EndEffector
 
 class DRV90L():
     """The "DRV90L" class contains methods to control the robot.
@@ -364,15 +365,31 @@ class DRV90L():
 
 if __name__ == "__main__":
     s = DRV90L()
-    p1 = s.getSavedToolPose("HomePos")
-    m = reset_errors()
-    m.on = "true"
-    s.resetErrors(m)
-    s.enableRobot()
+    e = EndEffector()
+    Home = s.getSavedToolPose("Home")
+    p1 = s.getSavedToolPose("p1")
+    p2 = s.getSavedToolPose("p2")
+    p3 = s.getSavedToolPose("p3")
+    p4 = s.getSavedToolPose("p4")
+    END = s.getSavedToolPose("END")
     while not rospy.is_shutdown():
         print("Starting loop")
+        e.stopDispensing()
         time.sleep(1)
-        s.sendPositionMove(p1[0], p1[1], p1[2], p1[3], p1[4], p1[5], 100, 'world')
-        s.sendPositionMove(p1[0], p1[1]-100, p1[2]-150, p1[3], p1[4], p1[5], 100, 'world')
-        s.sendArcMove([p1[0]-100, p1[1]+50, p1[2]-150, p1[3], p1[4], p1[5]],[p1[0], p1[1]+200, p1[2]-150, p1[3], p1[4], p1[5]])
-        s.sendArcMove([p1[0]+100, p1[1]+50, p1[2]-150, p1[3], p1[4], p1[5]], [p1[0], p1[1]-100, p1[2]-150, p1[3], p1[4], p1[5]])
+        s.sendPositionMove(Home[0], Home[1], Home[2], Home[3], Home[4], Home[5], 50, 'world')
+        e.stopBrushing()
+        time.sleep(1)
+        e.startBrushing()
+        s.sendPositionMove(p1[0], p1[1], p1[2], p1[3], p1[4], p1[5], 50, 'world')
+        e.startDispensing()
+        s.sendArcMove([p2[0], p2[1], p2[2], p2[3], p2[4], p2[5]], [p3[0], p3[1], p3[2], p3[3], p3[4], p3[5]])
+        time.sleep(1)
+        s.sendArcMove([p4[0], p4[1], p4[2], p4[3], p4[4], p4[5]], [p1[0], p1[1], p1[2], p1[3], p1[4], p1[5]])
+        time.sleep(1)
+        e.stopBrushing()
+        e.stopDispensing()
+        s.sendPositionMove(Home[0], Home[1], Home[2], Home[3], Home[4], Home[5], 50, 'world')
+        time.sleep(1)
+        e.startBrushing()
+        e.startDispensing()
+        s.sendPositionMove(END[0], END[1], END[2], END[3], END[4], END[5], 50, 'world')
